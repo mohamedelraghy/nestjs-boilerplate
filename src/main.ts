b65f623c7219bc0cf640c7b2c678fa3637dfef93
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-02-17 20:24:04 +0200

    initiat nest server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+
+  const port = process.env.PORT || config.port || 3000;
+  await app.listen(port, () => {
+    Logger.verbose(`🚀 Server listening on prot:${port} at ${config.nodeEnv}`);
+  });
 }
+
 bootstrap();
