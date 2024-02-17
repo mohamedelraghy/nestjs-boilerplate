c8333dfbaf9e220196ffd5e83d7bba6e63138c5c
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-02-17 19:08:14 +0200

    import ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { ConfigModuleConfig } from './config/options/config.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRootAsync(ConfigModule, { useClass: ConfigModuleConfig }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
