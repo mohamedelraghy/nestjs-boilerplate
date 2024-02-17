007f750b74267aa69a26a440a5f6ec6f929a355a
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-02-17 19:10:19 +0200

    create config Module options

---
+++ src/config/options/config.config.ts
@@ -0,0 +1,15 @@
+import { ModuleConfigFactory } from '@golevelup/nestjs-modules';
+import { ConfigModuleOptions } from 'src/config/interfaces/config-options.interface';
+
+export class ConfigModuleConfig
+  implements ModuleConfigFactory<ConfigModuleOptions>
+{
+  createModuleConfig(): ConfigModuleOptions {
+    const env = process.env.NODE_ENV;
+
+    return {
+      fileName: !env ? '.env.development' : `.env.${env}`,
+      useProcess: false,
+    };
+  }
+}
