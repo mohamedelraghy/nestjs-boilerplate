4ea65b00728ff6e520533a1e8ab8190fa9d89b34
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-02-29 01:29:21 +0200

    create users Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { ConfigModuleConfig } from './config/options/config.config';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRootAsync(ConfigModule, { useClass: ConfigModuleConfig }),
     AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
