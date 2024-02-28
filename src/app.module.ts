5ad63a31a66ababef488b48332520c0a3132c815
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-02-29 01:24:48 +0200

    create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { ConfigModuleConfig } from './config/options/config.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRootAsync(ConfigModule, { useClass: ConfigModuleConfig }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
