c73ed42bde0ab804a54edecd0d92711b2b6ac3ac
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-03-01 00:47:05 +0200

    create sigin in services and controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  controllers: [AuthController],
+  imports: [UsersModule],
   providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signIn(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+
+    if (user?.password !== password)
+      throw new UnauthorizedException('Invalid username or password');
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { pass, ...result } = user;
+
+    // TODO: Generate a JWT and return it here
+    // instead of the user object
+    return result;
+  }
+}
