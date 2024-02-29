e235c07b08c7e0976c075351f5e3749c44f7c077
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-03-01 00:46:11 +0200

    create user service to find one user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
+
+  async findOne(usrename: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === usrename);
+  }
+}
