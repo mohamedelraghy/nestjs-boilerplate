7353a19b37b69bd5ab224f6cf7205e47f134b3ab
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-03-01 20:56:50 +0200

    add: service to validate user for local strategy and clean up signIn service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
@@ -9,13 +9,19 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(username: string, password: string): Promise<any> {
+  async signIn(user: any): Promise<any> {
+    const payload = { username: user.username, sub: user.userId };
+    return { access_token: await this.jwtService.signAsync(payload) };
+  }
+
+  async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(username);
 
-    if (user?.password !== password)
-      throw new UnauthorizedException('Invalid username or password');
-    const payload = { sub: user.userId, username: user.username };
-
-    return { access_token: await this.jwtService.signAsync(payload) };
+    if (user && user.password === pass) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...res } = user;
+      return res;
+    }
+    return null;
   }
 }
