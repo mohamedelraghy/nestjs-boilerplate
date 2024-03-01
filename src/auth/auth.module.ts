880789aa00c10cb7078b371c66b782abc896be25
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-03-01 20:55:46 +0200

    import modules and provide strategies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,17 +4,20 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constant';
+import { LocalStrategy } from './strategies/local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     UsersModule,
+    PassportModule,
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '60s' },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
   exports: [AuthService],
 })
