f6b4ae99932d863f93168f05cb74e80d5777eb59
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-03-01 16:40:08 +0200

    add JwtServices

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,21 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async signIn(username: string, password: string): Promise<any> {
     const user = await this.usersService.findOne(username);
 
     if (user?.password !== password)
       throw new UnauthorizedException('Invalid username or password');
+    const payload = { sub: user.userId, username: user.username };
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { pass, ...result } = user;
-
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
-    return result;
+    return { access_token: await this.jwtService.signAsync(payload) };
   }
 }
