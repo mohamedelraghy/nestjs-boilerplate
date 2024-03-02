0510d9ca336ad863e30473e65bbc6671eb9008b9
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-03-02 12:49:31 +0200

    use cookieParser() and CookieSession() as a middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import cookieSession = require('cookie-session');
 
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
@@ -7,6 +9,17 @@ import { ConfigService } from './config/config.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
+
+  app.use(cookieParser());
+  app.use(
+    cookieSession({
+      name: 'session',
+      keys: ['secret'],
+      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+      sameSite: 'none',
+      secure: false,
+    }),
+  );
 
   const port = process.env.PORT || config.port || 3000;
   await app.listen(port, () => {
