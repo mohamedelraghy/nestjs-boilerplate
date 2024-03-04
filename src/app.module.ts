251166d610f61ebb597ca5469a5337324eab92d4
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-03-04 23:25:25 +0200

    import mongoose Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,16 @@ import { ConfigModule } from './config/config.module';
 import { ConfigModuleConfig } from './config/options/config.config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModuleConfig } from './config/options/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRootAsync(ConfigModule, { useClass: ConfigModuleConfig }),
+    MongooseModule.forRootAsync({
+      useClass: MongooseModuleConfig,
+      imports: [ConfigModule.Deferred],
+    }),
     AuthModule,
     UsersModule,
   ],
