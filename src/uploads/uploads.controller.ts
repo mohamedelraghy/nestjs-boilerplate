92cfb3964ee37f62c12e5e0d74fa4fbb895be1f2
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-03-16 02:42:48 +0200

    build route handler for upload file

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -1,5 +1,8 @@
 import {
   Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
   Post,
   UploadedFile,
   UseInterceptors,
@@ -7,11 +10,25 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
 
+import { UploadsService } from './uploads.service';
+
 @Controller('uploads')
 export class UploadsController {
+  constructor(private readonly uploadService: UploadsService) {}
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 5 * 1024 * 1024 }),
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log({ file });
+    return this.uploadService.saveFile(file);
   }
 }
