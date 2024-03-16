7d8958bc0497e607485f96a3f86420903d33670d
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-03-16 02:43:23 +0200

    import multer module

--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+
 import { UploadsController } from './uploads.controller';
+import { MulterModuleConfig } from 'src/config/options/multer.config';
+import { UploadsService } from './uploads.service';
 
 @Module({
-  controllers: [UploadsController]
+  controllers: [UploadsController],
+  imports: [MulterModule.registerAsync({ useClass: MulterModuleConfig })],
+  providers: [UploadsService],
 })
 export class UploadsModule {}
