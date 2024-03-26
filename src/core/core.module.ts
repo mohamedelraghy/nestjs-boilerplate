84a78c2f8ce77fb2796c06084d18fc3e098fabcb
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-03-26 02:27:22 +0200

    provide app filter

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
 import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 @Module({
-  providers: [{ provide: APP_INTERCEPTOR, useClass: LoggingInterceptor }],
+  providers: [
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+  ],
 })
 export class CoreModule {}
